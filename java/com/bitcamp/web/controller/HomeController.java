fb49d154849a0df23091f1db11acb6c921413356
pakjkwan <pakjkwan@gmail.com>
2019-06-11 17:32:17 +0900

    17:32

---
+++ java/com/bitcamp/web/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.bitcamp.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * HomeController
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")   
+    public String index() {
+        System.out.println("루트 URL 경로로 들어옴");
+        return "index";
+    }
+  
+}
