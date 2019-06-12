1a384cdf00b7e97d1f6da3e9c059b0fbc5f5b2c4
pakjkwan <pakjkwan@gmail.com>
2019-06-12 14:27:04 +0900

    14:27

--- resources/static/resources/js/index.js
+++ resources/static/resources/js/index.js
@@ -3,7 +3,7 @@ var app = (function(){
         login_form();
     }
     let login_form = function(){
-        var wrapper = document.querySelector('#wrapper');
+        let wrapper = document.querySelector('#wrapper');
         wrapper.innerHTML = '<form action="/action_page.php">'
         +'  First name:<br>'
         +'  <input type="text" name="firstname" value="Mickey">'
@@ -11,22 +11,34 @@ var app = (function(){
         +'  Last name:<br>'
         +'  <input type="text" name="lastname" value="Mouse">'
         +'  <br><br>'
-        +'  <input type="submit" value="Submit">'
+        +'  <input id="login-btn" type="button" value="LOGIN">'
+        +'  <input id="join-btn" type="button" value="JOIN">'
         +'</form> ';
-        
-        var btn = document.querySelector('#btn');
-        var target = document.getElementById('target');
-        btn.addEventListener('click',function(){
-            target.innerHTML = '';
+        let join_btn = document.querySelector('#join-btn');
+        join_btn.addEventListener('click',function(){
+            join_form();
         });
-        var btn2 = document.querySelector('#btn2');
         
-        btn2.addEventListener('click', function(){
-            var temp = document.createTextNode('새로 추가됨');
-            target.appendChild(temp);
-        });
+    }
+    let join_form = function(){
+        let wrapper = document.querySelector('#wrapper');
+        wrapper.innerHTML = '<form>'
+        +'	ID<br>'
+        +'	<input type="text" name="id"><br>'
+        +'	PW<br>'
+        +'	<input type="password" name="pw"><br>'
+        +'	이름<br>'
+        +'	<input type="text" name="name"><br>'
+        +'	주민번호<br>'
+        +'	<input type="password" name="ssn"><br>'
+        +'	전화번호<br>'
+        +'	<input type="text" name="phone"><br><br>'
+        +'	<input id="btn3" type="submit" value="확인">'
+        +'	<input id="btn4" type="reset" value="취소">'
+        +'</form>';
     }
     return {init : init};
 })();
+
 
 
